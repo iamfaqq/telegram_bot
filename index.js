c31454e5e2d73b6c320148210ddfda4dce6f81a4
iamfaqq <iamfaqq@gmail.com>
2019-05-30 19:06:07 +0300

    bot complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TOKEN = '714194235:AAFHU5yPOdVWuRPqnyoP6iGmNKhr_nBYoPs'
 
 const TelegramBot = require('node-telegram-bot-api')
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){ res.end('') })
 const bot = new TelegramBot(TOKEN, {polling:true})
 
 bot.on('message', msg => {
